6c157d5ef34a225ceeec96778bc2eedbc34c70a4
Akarsh Simha <akarshsimha@gmail.com>
2024-07-25 15:40:11 -0700

    Handy scripts to bump CSS and JavaScript versions
    These scripts simply change the URL of the stylesheet/script by
    passing an ignored GET parameter to them, like ?version=4. The server
    ignores the `version` parameter and simply returns the file, but the
    client thinks that it is a new URL and therefore hits the server even
    when it has an old version cached. This forces the same effect as
    evicting that file from the cache on the browser.

---
+++ scripts/bump_js_ver.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+
+if [[ $(basename $(pwd)) != "docs" ]]; then
+    >&2 echo "Please execute in the docs/ folder"
+    exit 1;
+fi;
+
+if [ -z $1 ]; then
+    >&2 echo "Usage: $0 <new version number>"
+    exit 1;
+fi;
+
+ver=$1
+
+echo "Updating to version ${ver}"
+
+ls *.htm* _layouts/*.htm* _includes/*.htm* | ../scripts/parallel_perl.sh 's:common.js\?version=\d+:common.js?version='${ver}':'
