67e24c362eabcaa8f9891a73a5d8fb3ff931065b
Akarsh Simha <akarshsimha@gmail.com>
2024-12-17 21:57:16 -0800

    Improve (dso) postprocessing

--- scripts/dso_postproc_regex.sh
+++ scripts/dso_postproc_regex.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 perl -CSAD -i -pe 's,\(\s*dso([^)]*)\s*\\?\)((?:(?!\(dso).)+)\\?\(\s*dso\s*\),my $temp = $1; my $tmp2 = $2; $temp =~ s/simbad: *["\x{FF02}\x{201C}\x{201D}\x{201E}]?((?:.(?! omit| noindex))+)(?:["\x{FF02}\x{201C}\x{201D}\x{201E}]|\b)/my $temp3 = $1; $temp3 =~ s:\\([+-\.]):\1:g; "simbad=\"" . $temp3 . "\""/uge; $temp =~ s:\\$::; $tmp2 =~ s/\\([+-])/\1/g; " <x-dso" . $temp . ">" . $tmp2 . "</x-dso> ",ge' $@
+perl -i -pe 's:<x-dso([^>]*)>\*\*(.+)\*\*</x-dso>:<b><x-dso\1>\2</x-dso></b>:g' $@
 
