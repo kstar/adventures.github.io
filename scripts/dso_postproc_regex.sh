f2a3eac2ea5c346166e204f1c9116c7678dcd82d
Akarsh Simha <akarshsimha@gmail.com>
2024-12-17 21:02:11 -0800

    Improve perl regex for post-processing Steve's emails

--- scripts/dso_postproc_regex.sh
+++ scripts/dso_postproc_regex.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-perl -i -pe 's,\(\s*dso([^)]*)\s*\\?\)((?:(?!\(dso).)+)\\?\(\s*dso\s*\),my $temp = $1; my $tmp2 = $2; $temp =~ s/simbad:(.+)$/simbad="\1"/; $tmp2 =~ s/\\-/-/; "<x-dso" . $temp . ">" . $tmp2 . "</x-dso>",ge' $@
+perl -CSAD -i -pe 's,\(\s*dso([^)]*)\s*\\?\)((?:(?!\(dso).)+)\\?\(\s*dso\s*\),my $temp = $1; my $tmp2 = $2; $temp =~ s/simbad: *["\x{FF02}\x{201C}\x{201D}\x{201E}]?((?:.(?! omit| noindex))+)(?:["\x{FF02}\x{201C}\x{201D}\x{201E}]|\b)/my $temp3 = $1; $temp3 =~ s:\\([+-\.]):\1:g; "simbad=\"" . $temp3 . "\""/uge; $temp =~ s:\\$::; $tmp2 =~ s/\\([+-])/\1/g; " <x-dso" . $temp . ">" . $tmp2 . "</x-dso> ",ge' $@
+
